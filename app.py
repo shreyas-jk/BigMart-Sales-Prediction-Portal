c5431d71d46f8a1bed14072497a7303b76fb67bd
shreyaskudav <dekudav@gmail.com>
2021-09-13 19:40:45 +0530

    Added Logger

--- app.py
+++ app.py
@@ -24,45 +24,51 @@ def home():
 # Prediction API for single sale
 @app.route("/single_sales", methods=['POST'])
 def single_sales():
-    log.write_log('Single Sale API called')
-    if request.method == "POST":
-        request_data = request.get_json()   
-        log.write_log('Receiving data from API ')
-        outlet_type = request_data['outlet_type']
-        outlet_year = request_data['outlet_year']
-        item_type = request_data['item_type']
-        item_mrp = request_data['item_mrp']
-        item_weight = request_data['item_weight']
-        item_visibility = request_data['item_visibility']
-        selected_model = request_data['selected_model']
+    try:
+        log.write_log('Single Sale API called')
+        if request.method == "POST":
+            request_data = request.get_json()   
+            log.write_log('Receiving data from API ')
+            outlet_type = request_data['outlet_type']
+            outlet_year = request_data['outlet_year']
+            item_type = request_data['item_type']
+            item_mrp = request_data['item_mrp']
+            item_weight = request_data['item_weight']
+            item_visibility = request_data['item_visibility']
+            selected_model = request_data['selected_model']
 
-        sales = SingleSales(outlet_type, outlet_year, item_type, item_mrp, item_weight, item_visibility)
-        log.write_log('Sales object created successfully')
-        log.write_log('Prediction Process start')
-        result = predict_single(sales, get_model(selected_model))
-        log.write_log('API response sent')
-    else:
-        return None
+            sales = SingleSales(outlet_type, outlet_year, item_type, item_mrp, item_weight, item_visibility)
+            log.write_log('Sales object created successfully')
+            log.write_log('Prediction Process start')
+            result = predict_single(sales, get_model(selected_model))
+            log.write_log('API response sent')
+        else:
+            return None
+    except Exception as e:
+        log.write_log('Error -', str(e))
     return json.dumps(result[0])
 
 # Prediction API for Bulk Sales Data
 @app.route("/bulk_sales", methods=['GET', 'POST'])
 def bulk_sales():
-    log.write_log('Bulk Sales API called')
-    if request.method == "POST":
-        log.write_log('Receiving data from API ')
-        selected_model = request.form['selected_model']
-        file = request.files['file']
-        log.write_log('File received')
-        df = pd.read_csv(file)
-        log.write_log('File loaded as dataframe')
-        log.write_log('Prediction Process start')
-        result = predict_bulk(df, get_model(selected_model))
-        log.write_log('Formulating API response')
-        output = merge(df, result)
-        log.write_log('API response sent')
-    else:
-        return None
+    try:
+        log.write_log('Bulk Sales API called')
+        if request.method == "POST":
+            log.write_log('Receiving data from API ')
+            selected_model = request.form['selected_model']
+            file = request.files['file']
+            log.write_log('File received')
+            df = pd.read_csv(file)
+            log.write_log('File loaded as dataframe')
+            log.write_log('Prediction Process start')
+            result = predict_bulk(df, get_model(selected_model))
+            log.write_log('Formulating API response')
+            output = merge(df, result)
+            log.write_log('API response sent')
+        else:
+            return None
+    except Exception as e:
+        log.write_log('Error -', str(e))
     return Response(output.to_csv(), mimetype="text/csv", headers={"Content-disposition":"attachment; filename=filename.csv"})
 
 # Get and load saved model
